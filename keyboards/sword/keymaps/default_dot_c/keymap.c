59cd73154f7db2474c507e642acae9e8f71591ea
Daniel Hellsson <git@hellsson.com>
2024-05-25 21:16:51 +0200

    Left is low.

--- keyboards/sword/keymaps/default_dot_c/keymap.c
+++ keyboards/sword/keymaps/default_dot_c/keymap.c
@@ -26,6 +26,13 @@ enum layers {
 #define REDO LCTL(KC_Y)
 // (For OS X, you'll want to change these editing keys to LGUI(KC_Z) etc.)
 
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+  // If console is enabled, it will print the matrix position and status of each key pressed
+#ifdef CONSOLE_ENABLE
+    uprintf("KL: kc: 0x%04X, col: %2u, row: %2u, pressed: %u, time: %5u, int: %u, count: %u\n", keycode, record->event.key.col, record->event.key.row, record->event.pressed, record->event.time, record->tap.interrupted, record->tap.count);
+#endif 
+  return true;
+}
 
 // clang-format off
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
