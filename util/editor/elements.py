657fa06defb0ee4a886405098e89fd46072cb2c1
Daniel Hellsson <git@hellsson.com>
2024-06-02 16:40:02 +0200

    Add text element and list view.

--- util/editor/elements.py
+++ util/editor/elements.py
@@ -10,6 +10,7 @@ class Element:
     def draw(self):
         pass
 
+
 class Rectangle(Element):
     def __init__(self, x: int, y: int, width: int, height: int, color: Color):
         super().__init__(x, y, width, height)
@@ -17,3 +18,18 @@ class Rectangle(Element):
     
     def draw(self):
         draw_rectangle(self.x, self.y, self.width, self.height, self.color)
+
+
+class Text(Element):
+    def __init__(self, x: int, y: int, text: str, size: int, color: Color):
+        super().__init__(x, y, 0, 0)
+        self.text = text
+        self.size = size
+        self.color = color
+        self.width = measure_text(text, size)
+        self.height = size
+
+
+    def draw(self):
+        draw_text(self.text, self.x, self.y, self.size, self.color)
+
